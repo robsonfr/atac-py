317d25de9b9fc3bf01ab876c41774c839381bb40
robsonsfranca@gmail.com <robsonsfranca@gmail.com@426a0c32-9b08-11de-b37f-933587f4c9ea>
2011-08-21 06:41:58 +0000

    Added a distutils setup file. Now it is using py2exe to create an executable file... Next steps:   - Make the engine code look more like an engine   - All the states (opening, intro, menu, gameplay, ending, gameover)

--- trunk/atacpy/src/ourgame.py
+++ trunk/atacpy/src/ourgame.py
@@ -1,49 +1,23 @@
 import pygame, sys, math, random
-from pygame.locals import Color, Rect, KEYDOWN, KEYUP, QUIT
-from engine.graphics import Layer, Sprite
-from engine import data_load        
+from pygame.locals import Rect
+from engine.graphics import Sprite
+from engine import data_load, Game        
+from atacpy import Star
 
-class Star(Layer):
+class Ourgame(Game):
     
-#    modificadores = [0.25, 0.25, 0.05, 0.20, 0, 0.10, 0.05, 0.05, 0.05]
-    
-    def __reset(self):
-        self.x = random.randint(0,self.max_x - 1)
-        self.y = random.randint(0,self.max_y - 1)
-        self.color = Color(random.randint(0,255) * 16843008)
-    
-    def __init__(self, (max_x, max_y)):
-        self.max_x = max_x;
-        self.max_y = max_y
-        self.__reset()
-        self.speed = random.randint(2,5)
-
-    def draw(self, target):              
-        target.set_at((self.x, self.y), self.color)
-
-        if self.y + self.speed >= target.get_height():
-            old_y = self.y
-            self.__reset()
-            self.y = old_y
-        self.y = (self.y + self.speed) % target.get_height() 
-
-class Ourgame:
     def __init__(self):
-        pygame.init()
-        pygame.mixer.init()        
-        self.screenSize = (640, 480)
-        self.situacao = [0, 0, 0, 0, 0, 0]
-        self.screen = pygame.display.set_mode(self.screenSize)
+        Game.__init__(self)
+        
         self.fundo = data_load("fundo2.png")
         self.num_estrelas = 40
         self.som_tiro = data_load("ourgame_fx1.ogg")
-        pygame.mouse.set_visible(False)
+
 
     def load_bitmaps(self):
         self.background = pygame.Surface(self.screenSize)
-        self.sprite = Sprite(data_load("new_nave.png"))
-        self.padraoTiro = Sprite(data_load("random.png"))
-        self.navInimiga = Sprite(data_load("new_nave_inimig.png"))
+        lista_figuras = ["new_nave.png", "random.png","new_nave_inimig.png"]
+        self.sprite, self.padraoTiro, self.navInimiga = [Sprite(data_load(f)) for f in lista_figuras]
         self.posTiroX = 0
         self.posTiroY = 480
         self.limTiroY = 0
@@ -64,43 +38,17 @@ class Ourgame:
     def credits(self):
         pass
     
-    def input(self, event):
-        if event.type == QUIT:
-            sys.exit()        
-        if event.type == KEYDOWN:
-            if event.key == 27: # ESCAPE
-                self.situacao[5] = 1
-            if event.key == 97: # A
-                self.situacao[0] = 1
-            if event.key == 100: #D
-                self.situacao[1] = 1
-            if event.key == 119: #W
-                self.situacao[2] = 1
-            if event.key == 115: #S
-                self.situacao[3] = 1
-            if event.key == 32:
-                print pygame.display.toggle_fullscreen()
-            #print event.key
-        if event.type == KEYUP:
-            if event.key == 27: # ESCAPE
-                self.situacao[5] = -1
-            if event.key == 97: # A
-                self.situacao[0] = 0
-            if event.key == 100: #D
-                self.situacao[1] = 0
-            if event.key == 119: #W
-                self.situacao[2] = 0
-            if event.key == 115: #S
-                self.situacao[3] = 0
+    def end_game(self):
+        sys.exit()
             
     def updateScreen(self):
         
-        if self.situacao[0] == 1 and self.sprite.x >= 20 + self.sprite.stepx:
+        if Game.situacao[0] == 1 and self.sprite.x >= 20 + self.sprite.stepx:
             self.sprite.x -= self.sprite.stepx    
-        elif self.situacao[1] == 1 and self.sprite.x <= 448 - self.sprite.stepx:
+        elif Game.situacao[1] == 1 and self.sprite.x <= 448 - self.sprite.stepx:
             self.sprite.x += self.sprite.stepx    
             
-        if self.situacao[2] == 1 and self.posTiroY == 480:
+        if Game.situacao[2] == 1 and self.posTiroY == 480:
             self.posTiroY -= 20
             self.som_tiro.play(loops=-1)
             self.posTiroX = self.sprite.x + 14
@@ -111,12 +59,12 @@ class Ourgame:
             
     
         #
-        #if self.situacao[2] == 1 and self.sprite.y >= self.sprite.stepy:
+        #if Game.situacao[2] == 1 and self.sprite.y >= self.sprite.stepy:
         #    self.sprite.y -= self.sprite.stepy
-        #elif self.situacao[3] == 1 and self.sprite.y <= 464-self.sprite.stepy:
+        #elif Game.situacao[3] == 1 and self.sprite.y <= 464-self.sprite.stepy:
         #    self.sprite.y += self.sprite.stepy
             
-        if self.situacao[5] == -1:
+        if Game.situacao[5] == -1:
             sys.exit()
 
         yy = self.navInimiga.y + 1
