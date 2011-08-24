b655250c567916d528cfeb043d356da25a9c4c2e
robsonsfranca@gmail.com <robsonsfranca@gmail.com@426a0c32-9b08-11de-b37f-933587f4c9ea>
2011-08-24 06:19:54 +0000

    Added a brand new code for the Ship (Nave) TODO: Still in need for the opening intro and making the game engine look more like a game engine

--- trunk/atacpy/src/atacpy/__init__.py
+++ trunk/atacpy/src/atacpy/__init__.py
@@ -1,6 +1,7 @@
 import random
-from pygame.locals import Color
-from engine.graphics import Layer
+from pygame.locals import Color, Rect
+from engine.graphics import Layer, Sprite
+from engine import data_load
 
 class Star(Layer):
     
@@ -27,3 +28,31 @@ class Star(Layer):
             self.y = old_y
         self.y = (self.y + self.speed) % target.get_height() 
 
+class Tiro(Layer):
+    
+    def __init__(self, pos = (0,480), lim_y = 480):
+        self.padrao_tiro = Sprite(data_load("random.png"))
+        self.x, self.y = pos
+        self.lim_y = lim_y
+    
+    def draw(self, target):    
+        self.padrao_tiro.draw_fragment(target, Rect(random.random() * 630, self.y, 4, 480 - self.y))
+
+class Nave(Layer):
+    
+    def __init__(self):        
+        self.sprite = Sprite(data_load("new_nave.png"))
+        self.sprite.move_to(100, 440)        
+        Layer.__init__(self, (self.sprite.x, self.sprite.y), self.sprite.size)
+        self.tiro = Tiro()
+
+    def move_to(self, x, y):
+        self.sprite.move_to(x,y)
+        return self
+    
+    def draw(self, target):
+        self.sprite.draw(target)
+        
+    def move_x(self, dx=1):
+        if dx < 0 and self.sprite.x >= 20 + self.sprite.step_x: self.sprite.x -= self.sprite.step_x
+        elif dx > 0 and self.sprite.x <= 448 - self.sprite.step_x: self.sprite.x += self.sprite.step_x 
